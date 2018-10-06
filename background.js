d25864851d60a14dd4850b093d74748f73161f5a
Juleshwar Babu <bjuleshwar@gmail.com>
2018-10-07 03:58:57 +0530

    added sendMessage to trigger new note creation

--- background.js
+++ background.js
@@ -1,13 +1,32 @@
-function sendMessageToCreateNewNode(info, tab){
-    chrome.tabs.executeScript( tab.id, {
-        code: 
-    });
+function onClickHandler(info, tab){
+    if(info.menuItemId === "create_new_note"){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {
+                    action: "create-note"
+                },
+                function(response){
+                    if(response){
+                        console.log("The note has been created");
+                    } else {
+                        console.log("Something went wrong");
+                    }
+                }
+            )
+        })
+    }
 }
 
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
 chrome.runtime.onInstalled.addListener(_ => {
+    
+    //Add "Add new note" option
     chrome.contextMenus.create({
-        "onclick": sendMessageToCreateNewNode,
-        "title": __MSG_context_menu_title__,
+        "id": "create_new_note",
+        "title": "__MSG_context_menu_title__",
         "contexts": ["all"]
     });
+
 })
