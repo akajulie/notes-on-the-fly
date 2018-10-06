f9773b6f7725ef79f345b1014686f91c565f6471
Juleshwar Babu <bjuleshwar@gmail.com>
2018-10-07 03:58:03 +0530

    added message listeners to create new note
    creates a new note at the point of the double click

--- content.js
+++ content.js
@@ -1,13 +1,48 @@
-button.addEventListener("click", function() {
-    alert(greeting + button.person_name + ".");
-}, false);
+let x = null;
+let y = null;
 
+function onMouseUpdate(e) {
+    console.log(e.pageX, e.pageY);
+    
+    x = e.pageX;
+    y = e.pageY;
+}
 
-function createNewNote(){
+function getPositionX() {
+    return x;
+}
+
+function getPositionY() {
+    return y;
+}
+
+function createNewNote(positionX, positionY){
     let note = document.createElement("textarea");
     note.style.position = "fixed";
-    note.style.left = 0;
-    note.style.top = 0;
+    note.style.left = positionX + "px";
+    note.style.top = positionY + "px";
     note.style.width = "100px";
     note.style.height = "100px";
+    return note;
 }
+
+document.addEventListener('contextmenu', onMouseUpdate, false);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        switch(request.action) {
+            case "create-note": 
+                document.body.appendChild(
+                    createNewNote(
+                        this.getPositionX(), 
+                        this.getPositionY()
+                    )
+                );
+                sendResponse(true);
+                break;
+            
+            default:
+                break;
+        }
+    }
+)
