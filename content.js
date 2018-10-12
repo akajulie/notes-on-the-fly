a7e7fbbca789c29f2b04820fd8e52a2e2bd9a98b
aka_julie <juleshwar.babu@zohocorp.com>
2018-10-12 09:02:55 +0530

    Note gets added to the page and delete button works

--- content.js
+++ content.js
@@ -1,9 +1,11 @@
+let count = 0;
+let ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 let x = null;
 let y = null;
 
 function onMouseUpdate(e) {
     console.log(e.pageX, e.pageY);
-    
     x = e.pageX;
     y = e.pageY;
 }
@@ -16,14 +18,48 @@ function getPositionY() {
     return y;
 }
 
-function createNewNote(positionX, positionY){
-    let note = document.createElement("textarea");
-    note.style.position = "fixed";
+function createNewNote(id){
+    let note = `
+                <div class="meow" id=`+ id +`>
+                    <header style="height: auto">
+                        <h1>Meow</h1>
+                    </header>
+                    <main>
+                        <button id="NOTF-delete-button">Delet Tis</button>
+                        <textarea name="note" id="note-textarea" cols="30" rows="10">
+                        Hello there, General n00bie
+                        </textarea>
+                    </main>
+                </div>
+                `;
+    console.log("note", note);
+    return note;
+}
+
+function getNoteID() {
+    return ids[count++];
+}
+
+function insertNoteIntoDOM(note) {
+    document.body.insertAdjacentHTML( "beforebegin", note);
+}
+
+function meowTheNote(note) {
+    setNotePosition(note, getPositionX(), getPositionY());
+    addDeleteButtonFunctionality(note);
+}
+
+function setNotePosition(note, positionX, positionY) {
     note.style.left = positionX + "px";
     note.style.top = positionY + "px";
-    note.style.width = "100px";
-    note.style.height = "100px";
-    return note;
+}
+
+function addDeleteButtonFunctionality(note) {
+    note.querySelector("#NOTF-delete-button").onclick = function(){ deleteNote(note) };
+}
+
+function deleteNote(note) {
+    note.remove();
 }
 
 document.addEventListener('contextmenu', onMouseUpdate, false);
@@ -31,16 +67,16 @@ document.addEventListener('contextmenu', onMouseUpdate, false);
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
         switch(request.action) {
-            case "create-note": 
-                document.body.appendChild(
-                    createNewNote(
-                        this.getPositionX(), 
-                        this.getPositionY()
-                    )
-                );
+            case "create-note": {
+                const id = getNoteID();
+                insertNoteIntoDOM(createNewNote(id));
+
+                let note = document.getElementById(id);
+                meowTheNote(note);
                 sendResponse(true);
                 break;
-            
+            }
+
             default:
                 break;
         }
